5f602d5bf5908fe09bfcf8eba134479d334ead09
pavel <uppaal@mail.ru>
2021-09-07 17:41:09 +0300

    refactoring

--- CoreTypes/SignalServiceClasses/StrategyScheduleRestrictors.cs
+++ CoreTypes/SignalServiceClasses/StrategyScheduleRestrictors.cs
@@ -5,62 +5,79 @@ namespace CoreTypes.SignalServiceClasses
 {
     public class StrategyScheduleRestrictors
     {
+        #region nested classes
         class InstrumentStateHolder
         {
-            public readonly List<StrategyRestrictor> ReferredStrategies;
-            public TradingRestriction Restriction;
+            public readonly List<StrategyStateHolder> ReferredStrategies;
+            public bool IsOutOfMarket = true;
 
             public InstrumentStateHolder()
             {
                 ReferredStrategies = new();
             }
-            public void ApplyRestriction(TradingRestriction restriction)
+
+            public bool SetOutOfMarket(bool isOutOfMarket)
             {
-                if (restriction != Restriction)
+                if (IsOutOfMarket != isOutOfMarket)
                 {
-                    Restriction = restriction;
-                    ReferredStrategies.ForEach(s => s.SetCheckFlag());
+                    IsOutOfMarket = isOutOfMarket;
+                    ReferredStrategies.ForEach(s => s.ToUpdate());
+                    return true;
                 }
+                return false;
             }
         }
-        class StrategyRestrictor
+        class StrategyStateHolder
         {
             private readonly int _strategyId;
-            private bool _checkFlag=true;
-            private TradingRestriction _lastRestriction = TradingRestriction.SoftStop;
             public readonly List<InstrumentStateHolder> UsedMarkets = new();
-            public StrategyRestrictor(int strategyId)
+
+            private bool _updateNeeded = true;
+            private bool _isOutOfMarket;
+            public StrategyStateHolder(int strategyId)
             {
                 _strategyId = strategyId;
             }
 
-            public void SetCheckFlag()
+            public void ToUpdate()
             {
-                _checkFlag = true;
+                _updateNeeded = true;
             }
 
             public ICommand GetCommand()
             {
-                if (!_checkFlag) return null;
+                if (!_updateNeeded) return null;
 
-                _checkFlag = false;
-                TradingRestriction newRestriction = UsedMarkets.Max(mkt => mkt.Restriction);
-                if (newRestriction == _lastRestriction) return null;
+                _updateNeeded = false;
+                bool newval_IsOutOfMarket = UsedMarkets.Any(mkt => mkt.IsOutOfMarket);
+                if (newval_IsOutOfMarket == _isOutOfMarket) return null;
 
-                _lastRestriction = newRestriction;
-                return new RestrictionCommand(CommandDestination.Strategy, CommandSource.Scheduler, _strategyId, newRestriction);
+                _isOutOfMarket = newval_IsOutOfMarket;
+                return new RestrictionCommand(CommandDestination.Strategy, CommandSource.OutOfMarket, _strategyId,
+                    _isOutOfMarket ? TradingRestriction.HardStop : TradingRestriction.NoRestrictions);
+            }
+            public ICommand GetCommandFirstTime()
+            {
+                //  when called first time, return command for all strategies
+                _updateNeeded = false;
+                _isOutOfMarket = UsedMarkets.Any(mkt => mkt.IsOutOfMarket);
+                return new RestrictionCommand(CommandDestination.Strategy, CommandSource.OutOfMarket, _strategyId,
+                    _isOutOfMarket ? TradingRestriction.HardStop : TradingRestriction.NoRestrictions);
             }
         }
-
+        #endregion
         private readonly Dictionary<string, InstrumentStateHolder> _instrumentsMap;
-        private readonly List<StrategyRestrictor> _strategyRestrictors;
+        private readonly List<StrategyStateHolder> _strategyRestrictors;
+        private bool _firstTime = true;
+        private bool _anyStateChanged;
+
         public StrategyScheduleRestrictors(List<(int, List<string>)> listOf_strategyWithUsedInstruments)
         {
             _instrumentsMap = new Dictionary<string, InstrumentStateHolder>();
-            _strategyRestrictors = new List<StrategyRestrictor>();
+            _strategyRestrictors = new List<StrategyStateHolder>();
             foreach (var strategyId_usedMarkets in listOf_strategyWithUsedInstruments)
             {
-                StrategyRestrictor sr = new StrategyRestrictor(strategyId_usedMarkets.Item1);
+                StrategyStateHolder sr = new StrategyStateHolder(strategyId_usedMarkets.Item1);
                 _strategyRestrictors.Add(sr);
 
                 foreach (string mkt in strategyId_usedMarkets.Item2)
@@ -72,23 +89,27 @@ namespace CoreTypes.SignalServiceClasses
                     sr.UsedMarkets.Add(instrumentHolder);
                 }
             }
-
-            _toCheckFlag = true;
         }
 
-        private bool _toCheckFlag;
         public void ApplyNewMarketRestrictions(List<(string, TradingRestriction)> tCommands)
         {
             foreach(var item in tCommands)
                 if (_instrumentsMap.TryGetValue(item.Item1.ToUpper(), out var instrumHolder))
-                    instrumHolder.ApplyRestriction(item.Item2);
-            //throw new System.NotImplementedException();
+                    if (instrumHolder.SetOutOfMarket(item.Item2 != TradingRestriction.NoRestrictions))
+                        _anyStateChanged = true;
         }
+
         public List<ICommand> GetCommands()
         {
-            if (!_toCheckFlag) return null;
+            if (_firstTime)
+            {
+                _firstTime = false;
+                _anyStateChanged = false;
+                return _strategyRestrictors.Select(s => s.GetCommandFirstTime()).ToList();
+            }
 
-            _toCheckFlag = false;
+            if (!_anyStateChanged) return null;
+            _anyStateChanged = false;
             return _strategyRestrictors.Select(s => s.GetCommand()).Where(item => item != null).ToList();
         }
     }
